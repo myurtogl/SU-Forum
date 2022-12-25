c3b59a4f12d24dcf388fdb55b2b006566a48e75c
Tan Çetiner <cetinertan13@gmail.com>
2022-12-26 00:56:32 +0300

    26.12.2022

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 const AddForm = ({
   loading,
@@ -10,37 +10,39 @@ const AddForm = ({
 }) => {
   if (loading) return null
   return (
-    <form>
-      {!isOwner ? (
-        <>
-          <label>
-            New Message:
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-          </label>
-          <button type="button" onClick={addMessage}>
-            Add Message
-          </button>{" "}
-        </>
-      ) : (
-        <>
-          <label>
-            Address to Add:
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-          </label>
-          <button type="button" onClick={addMember}>
-            Add New Member
-          </button>
-        </>
-      )}
-    </form>
+    <div style={{ margin: '20px' }}>
+      <form>
+        {!isOwner ? (
+          <>
+            <label>
+              New Message:
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            </label>
+            <button type="button" onClick={addMessage}>
+              Add Message
+            </button>{' '}
+          </>
+        ) : (
+          <>
+            <label>
+              Address to Add:
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            </label>
+            <button type="button" onClick={addMember}>
+              Add New Member
+            </button>
+          </>
+        )}
+      </form>
+    </div>
   )
 }
 
